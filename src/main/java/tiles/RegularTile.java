543d114f6d3308037105767f921f65613ee63904
MarieeMorte <nika5000.com@mail.ru>
2024-04-19 05:48:35 +0300

    fix codestyle

--- src/main/java/tiles/RegularTile.java
+++ src/main/java/tiles/RegularTile.java
@@ -1,7 +1,7 @@
 package tiles;
 
-import javax.swing.*;
 import java.util.Objects;
+import javax.swing.ImageIcon;
 
 public class RegularTile extends AbstractTile {
 
@@ -12,46 +12,40 @@ public class RegularTile extends AbstractTile {
     @Override
     public ImageIcon getUnselectedTileIcon() {
         TileTypes tileType = getTileType();
-        ImageIcon icon;
 
-        switch (tileType) {
-            case BLUE ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/blue.png")));
-            case GREEN ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/green.png")));
-            case GRAY ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/gray.png")));
-            case INDIGO ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/indigo.png")));
-            case RED ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/red.png")));
-            default ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/yellow.png")));
-        }
-
-        return icon;
+        return switch (tileType) {
+            case BLUE -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/blue.png")));
+            case GREEN -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/green.png")));
+            case GRAY -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/gray.png")));
+            case INDIGO -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/indigo.png")));
+            case RED -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/red.png")));
+            default -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/unselectedTiles/yellow.png")));
+        };
     }
 
     @Override
     public ImageIcon getSelectedTileIcon() {
         TileTypes tileType = getTileType();
-        ImageIcon icon;
 
-        switch (tileType) {
-            case BLUE ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/blue.png")));
-            case GREEN ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/green.png")));
-            case GRAY ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/gray.png")));
-            case INDIGO ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/indigo.png")));
-            case RED ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/red.png")));
-            default ->
-                    icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/yellow.png")));
-        }
-
-        return icon;
+        return switch (tileType) {
+            case BLUE -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/blue.png")));
+            case GREEN -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/green.png")));
+            case GRAY -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/gray.png")));
+            case INDIGO -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/indigo.png")));
+            case RED -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/red.png")));
+            default -> new ImageIcon(
+                    Objects.requireNonNull(getClass().getResource("/icons/selectedTiles/yellow.png")));
+        };
     }
 }
